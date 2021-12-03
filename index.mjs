75079cc7452c5f3d6ca4e23f1f0380fefe202a53
James Barr <sickspamemail@gmail.com>
2021-12-02 23:55:18 -0600

    rw - init

--- index.mjs
+++ index.mjs
@@ -5,8 +5,6 @@ import express from 'express';
 import serverless from 'serverless-http';
 const app = express();
 
-export const handler = serverless(app);
-
 import mongoose from 'mongoose';
 mongoose.connect(process.env.MONGO_URI, () => console.log('Connected to db'));
 
@@ -17,6 +15,4 @@ app.get('/', (req, res) => {
     res.render('index', { user: req.user });
 });
 
-app.listen(process.env.PORT || 8000, () =>
-    console.log(`Now listening on port ${process.env.PORT || 8000}`)
-);
+export const handler = serverless(app);
