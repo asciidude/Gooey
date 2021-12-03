54ca1227cac739b846701b6a0275d498fe4fc215
James Barr <sickspamemail@gmail.com>
2021-12-02 23:23:54 -0600

    rw - init

---
+++ index.mjs
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express';
+const app = express();
+
+import mongoose from 'mongoose';
+mongoose.connect(process.env.MONGO_URI, () => console.log('Connected to db'));
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('index', { user: req.user });
+});
+
+app.listen(process.env.PORT || 8000, () =>
+    console.log(`Now listening on port ${process.env.PORT || 8000}`)
+);
+
+export default app;
